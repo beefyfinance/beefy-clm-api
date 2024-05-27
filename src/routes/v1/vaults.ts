c56a68d914f57749511adf2e49eb4a958cdbf67c
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-05-27 21:49:18 +0100

    /vaults/{chain}/harvests/{since}

--- src/routes/v1/vaults.ts
+++ src/routes/v1/vaults.ts
@@ -9,12 +9,14 @@ import { pick } from 'lodash';
 import { VaultsQuery } from '../../../.graphclient/index';
 import { calculateLastApr, prepareAprState } from '../../utils/apr';
 import { interpretAsDecimal } from '../../utils/decimal';
+import { PreparedVaultHarvest, prepareVaultHarvests } from './vault';
 
 export default async function (
   instance: FastifyInstance,
   _opts: FastifyPluginOptions,
   done: (err?: Error) => void
 ) {
+  // vaults data for use by main api
   {
     type UrlParams = {
       chain: ChainId;
@@ -41,6 +43,45 @@ export default async function (
       const result = await getVaults(chain, period);
       reply.send(result);
     });
+  }
+
+  // Vaults harvest data
+  {
+    type UrlParams = {
+      chain: ChainId;
+      since: number;
+    };
+
+    const urlParamsSchema = S.object()
+      .prop(
+        'chain',
+        chainSchema.required().description('The chain to return vaults harvest data for')
+      )
+      .prop(
+        'since',
+        S.number().required().description('The unix timestamp to return harvests since')
+      );
+
+    const responseSchema = S.array().items(S.object());
+
+    const schema: FastifySchema = {
+      tags: ['v1'],
+      params: urlParamsSchema,
+      response: {
+        200: responseSchema,
+      },
+    };
+
+    instance.get<{ Params: UrlParams }>(
+      '/:chain/harvests/:since',
+      { schema },
+      async (request, reply) => {
+        const { chain, since } = request.params;
+
+        const result = await getVaultsHarvests(chain, since);
+        reply.send(result);
+      }
+    );
   }
 
   done();
@@ -88,4 +129,28 @@ const getVaultApy = (vault: VaultsQuery['beefyCLVaults'][0], periodSeconds: numb
   );
 
   return calculateLastApr(aprState, periodSeconds * 1000, now);
+};
+
+const getVaultsHarvests = async (chain: ChainId, since: number) => {
+  const rawVaults = await sdk
+    .VaultsHarvests({ since: since.toString() }, { chainName: chain })
+    .catch((e: unknown) => {
+      throw new GraphQueryError(e);
+    });
+
+  return rawVaults.beefyCLVaults.reduce(
+    (acc, vault) => {
+      if (vault.harvests.length === 0) {
+        return acc;
+      }
+
+      acc.push({
+        vaultAddress: vault.vaultAddress,
+        harvests: prepareVaultHarvests(vault),
+      });
+
+      return acc;
+    },
+    [] as { vaultAddress: string; harvests: PreparedVaultHarvest[] }[]
+  );
 };
