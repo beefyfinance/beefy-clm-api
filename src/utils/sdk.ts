3e78ea4bca4761ea0b948e2d892ee8aeb6b89b74
Clément <998369+prevostc@users.noreply.github.com>
2024-06-17 01:04:19 +0200

    Revert "Use a lighter graphql client"

--- src/utils/sdk.ts
+++ src/utils/sdk.ts
@@ -1,46 +1,25 @@
-import { ChainId, allChainIds } from '../config/chains';
-import { GraphQLClient } from 'graphql-request';
-import { Sdk, getSdk } from '../queries/codegen/sdk';
 import { createCachedFactoryByChainId } from './factory';
-import { GraphQueryError } from './error';
+import { ChainId } from '../config/chains';
+import { getMesh } from '@graphql-mesh/runtime';
+import { getMeshOptions, getSdk } from '../../.graphclient';
 
-// adds the chainId to the response on all sdk queries
-type EndpointSdk = {
-  [key in keyof Sdk]: (
-    ...args: Parameters<Sdk[key]>
-  ) => Promise<Awaited<ReturnType<Sdk[key]>> & { chain: ChainId }>;
-};
-
-export const getAllSdks = () => allChainIds.flatMap(chain => getSdksForChain(chain));
-
-export const getSdksForChain = createCachedFactoryByChainId((chain: ChainId): EndpointSdk[] => {
-  const clients = [
-    new GraphQLClient(
-      `https://api.goldsky.com/api/public/project_clu2walwem1qm01w40v3yhw1f/subgraphs/beefy-clm-${chain}/latest/gn`
-    ),
-  ];
-
-  if (chain === 'arbitrum') {
-    clients.push(
-      new GraphQLClient(
-        `https://api.goldsky.com/api/public/project_clu2walwem1qm01w40v3yhw1f/subgraphs/beefy-clm-${chain}-beta/latest/gn`
-      )
-    );
+export const getSdkForChain = createCachedFactoryByChainId(
+  <TGlobalContext = any, TOperationContext = { chainName: ChainId }>(chainId: ChainId) => {
+    return getMeshOptions()
+      .then(meshOptions => {
+        // beta is only available on arbitrum
+        if (chainId !== 'arbitrum') {
+          meshOptions.sources = meshOptions.sources.filter(
+            source => source.name !== 'beefy-clm-beta'
+          );
+        }
+        return getMesh(meshOptions);
+      })
+      .then(({ sdkRequesterFactory }) => {
+        return sdkRequesterFactory({ chainName: chainId });
+      })
+      .then(sdkRequester => {
+        return getSdk<TOperationContext, TGlobalContext>((...args) => sdkRequester(...args));
+      });
   }
-
-  return clients
-    .map(client => getSdk(client))
-    .map(sdk => {
-      return new Proxy(sdk, {
-        get:
-          (target, prop) =>
-          async (...args: any) => {
-            // @ts-ignore
-            const res = await target[prop](...args).catch((e: unknown) => {
-              throw new GraphQueryError(e);
-            });
-            return { ...res, chain };
-          },
-      }) as EndpointSdk;
-    });
-});
+);
