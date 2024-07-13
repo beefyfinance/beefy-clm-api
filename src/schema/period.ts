c3cc97752fcd97645c976e28b4c3df2e108dca8d
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-07-13 10:07:40 +0200

    Allow periods slightly larger than 1 day to include late harvests

--- src/schema/period.ts
+++ src/schema/period.ts
@@ -4,6 +4,7 @@ import { StringEnum } from '../utils/typebox';
 export enum Period {
   '1h' = '1h',
   '1d' = '1d',
+  '1.1d' = '1.1d',
 }
 
 export const allPeriodIds: Array<Period> = Object.values(Period);
@@ -13,6 +14,7 @@ export const periodAsKeySchema = Type.Enum(Period);
 const periodToSeconds = {
   '1h': 3600,
   '1d': 86400,
+  '1.1d': 95040,
 } as Record<Period, number>;
 
 export function getPeriodSeconds(period: Period) {
