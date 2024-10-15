6e6b60a66261298efda10247fe6afb4eae6e174c
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-10-16 00:15:50 +0200

    Add more periods

--- src/schema/period.ts
+++ src/schema/period.ts
@@ -5,6 +5,10 @@ export enum Period {
   '1h' = '1h',
   '1d' = '1d',
   '1.1d' = '1.1d',
+  '3d' = '3d',
+  '3.1d' = '3.1d',
+  '1w' = '1w',
+  '1.1w' = '1.1w',
 }
 
 export const allPeriodIds: Array<Period> = Object.values(Period);
@@ -15,6 +19,10 @@ const periodToSeconds = {
   '1h': 3600,
   '1d': 86400,
   '1.1d': 95040,
+  '3d': 259200,
+  '3.1d': 267840,
+  '1w': 604800,
+  '1.1w': 665280,
 } as Record<Period, number>;
 
 export function getPeriodSeconds(period: Period) {
